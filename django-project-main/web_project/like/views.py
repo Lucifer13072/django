ed568d3c20d7985eaff70631122ad3aad6b1ce2d
tytyber <at2823743@gmail.com>
2023-12-28 13:56:44 +0300

    like

---
+++ django-project-main/web_project/like/views.py
@@ -0,0 +1,31 @@
+from cgitb import html
+from django.shortcuts import render, redirect
+from okay.forms import PostForm
+from okay.models import Post
+
+def home(request):
+    posts = Post.objects.all()
+    return render(request, 'home.html',{
+        'posts': posts,
+        'title': 'Главная страница',
+    })
+
+
+def about(request):
+    return render(request, 'about.html')
+
+
+def base(request):
+    return render(request, 'base.html')
+
+
+def create(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            form.save()
+    form = PostForm()
+    content = {"form": form}
+    return render(request, 'create.html', content)
+
+
